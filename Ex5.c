d53751c234557968f5c46abd6c49ec5cf6371478
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-06 16:22:16 +0700

    1st

---
+++ Ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    
+        if(num<10); {
+        printf("too low",num);
+    }
+        elif(10<num<20);
+    {
+
+        printf("Correct");
+    }
+       else(num>20);
+    {
+        printf("Too high");
+    }
+    return 0;
+}
