01832ff8be547a27c4e812d320efe20986a5f21d
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-08 16:35:45 +0700

    1st

---
+++ Lesson01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    // char chr='b';
+    // int age =19;
+    // const float PI=3.14;
+
+    // printf("Character=%c\n",chr);
+    // printf ("Age =%d\n",age);
+    // printf("PI=%f",PI);
+    // return 0;
+    // char chr;
+    // printf("Enter the letter:");
+    // scanf("%c",&chr);
+    // printf("you enter %c",chr);
+    // return 0;
+    int a;
+    float b;
+    printf ("Enter an interger and then a float:");
+    scanf ("%d%f",&a,&b);
+    printf("you enteres %d and %g", a,b);
+}
