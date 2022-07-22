43a2c1d83a545152d756dd35adb9c4db09ca6363
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-22 17:48:22 +0700

    1st

--- Lesson01.c
+++ Lesson01.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 int main() {
     // char chr='b';
     // int age =19;
@@ -18,13 +20,16 @@ int main() {
     // printf ("Enter an interger and then a float:");
     // scanf ("%d%f",&a,&b);
     // printf("you enteres %d and %g", a,b);
-    int a =10,b=18;
-    printf("a+b %d\n", a+b );
-    printf("a-b %d\n", a-b );
-    printf("a*b %d\n", a*b );
-    printf("a/b %d\n", a/b );
-    printf("ab %d\n", a%b );
-    return 0;
+    // int a =10,b=18;
+    // printf("a+b %d\n", a+b );
+    // printf("a-b %d\n", a-b );
+    // printf("a*b %d\n", a*b );
+    // printf("a/b %d\n", a/b );
+    // printf("ab %d\n", a%b );
+    // int R=20;
+    // float A=pow(A,2)*M_PI;
+    // printf("A=%.2f cm2",A);
+    //  return 0;
 
 
 }
